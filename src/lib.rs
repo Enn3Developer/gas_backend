aa2c3d64a3d00cc4c01c2cf9caf25f06465d03ae
enn3 <arturpeshko39@gmail.com>
2023-05-05 18:15:14 +0200

    Added foods

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,21 @@
+#![feature(let_chains)]
+
 use serde_derive::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Serialize, Deserialize)]
+pub enum PriceUnit {
+    KG,
+    PZ,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct Food {
+    id: u32,
     image: String,
     name: String,
     price: f32,
+    // price_unit: PriceUnit,
     desc: String,
 }
 
@@ -14,11 +25,20 @@ pub struct Foods {
 }
 
 impl Food {
-    pub fn new(image: String, name: String, price: f32, desc: String) -> Self {
+    pub fn new(
+        id: u32,
+        image: String,
+        name: String,
+        price: f32,
+        // price_unit: PriceUnit,
+        desc: String,
+    ) -> Self {
         Self {
+            id,
             image,
             name,
             price,
+            // price_unit,
             desc,
         }
     }
@@ -37,6 +57,22 @@ impl Foods {
 pub fn common(page: String) -> String {
     let page = page.replace("$$FOOTER$$", include_str!("../html/footer"));
     let page = page.replace("$$HEAD$$", include_str!("../html/head"));
-
-    page
+    if page.contains("$$FOODS$$") {
+        if let Ok(food_data) = fs::read_to_string("data/food.toml") &&
+            let Ok(food_template) = fs::read_to_string("html/foods") {
+            let foods: Foods = toml::from_str(&food_data).unwrap();
+            let mut content = vec![];
+            for food in foods.foods {
+                let f = food_template.replace("$$IMAGE$$", &food.image);
+                let f = f.replace("$$NAME$$", &food.name);
+                let f = f.replace("$$PRICE$$", &format!("{:.2}", food.price));
+                content.push(f);
+            }
+            page.replace("$$FOODS$$", &content.join("\n"))
+        } else {
+            page
+        }
+    } else {
+        page
+    }
 }
