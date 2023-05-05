641f1736d8ef0ac1b4e561995a1ded0b2483e671
enn3 <arturpeshko39@gmail.com>
2023-05-05 18:43:18 +0200

    Optimized page serving

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(let_chains)]
-
 use serde_derive::{Deserialize, Serialize};
 use std::fs;
 
@@ -58,8 +56,8 @@ pub fn common(page: String) -> String {
     let page = page.replace("$$FOOTER$$", include_str!("../html/footer"));
     let page = page.replace("$$HEAD$$", include_str!("../html/head"));
     if page.contains("$$FOODS$$") {
-        if let Ok(food_data) = fs::read_to_string("data/food.toml") &&
-            let Ok(food_template) = fs::read_to_string("html/foods") {
+        if let Ok(food_data) = fs::read_to_string("data/food.toml") {
+            let food_template = include_str!("../html/foods");
             let foods: Foods = toml::from_str(&food_data).unwrap();
             let mut content = vec![];
             for food in foods.foods {
